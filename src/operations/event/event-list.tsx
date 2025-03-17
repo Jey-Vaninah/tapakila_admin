9346994a00b2c393dbb598df946c889e3264f312
tsilavina007 <andriamiart114@gmail.com>
2025-03-17 15:46:04 +0300

    ✨ feat: add Loading on List Resource

--- src/operations/event/event-list.tsx
+++ src/operations/event/event-list.tsx
@@ -1,43 +1,62 @@
+import { CircularProgress } from "@mui/material";
 import {
-  List,
-  Datagrid,
-  TextField,
-  DateField,
-  FunctionField,
-  DeleteButton,
-  EditButton,
+	List,
+	Datagrid,
+	TextField,
+	DateField,
+	FunctionField,
+	DeleteButton,
+	EditButton,
+	useListContext,
 } from "react-admin";
 import { FlexBox } from "../../common/components/flex-box";
-import { Event } from "../../providers";
+
 export const EventList = () => {
-  return (
-    <List>
-      <Datagrid bulkActionButtons={false}>
-        <TextField source="title" label="Title" />
-        <TextField source="slug" label="Slug" />
-        <DateField source="startDate" label="Start Date" showTime />
-        <TextField source="startTime" label="Start Time" />
-        <FunctionField
-          label="Actions"
-          render={() => {
-            return (
-              <FlexBox sx={{ justifyContent: "start", gap: 2 }}>
-                <DeleteButton />
-                <EditButton />
-              </FlexBox>
-            );
-          }}
-        />
-        {/* <FunctionField
-          label="Status"
-          render={(event: Event) => {
-            return (
-              event.status[0].toUpperCase() +
-              event.status.slice(1).toLocaleLowerCase()
-            );
-          }}
-        /> */}
-      </Datagrid>
-    </List>
-  );
+	return (
+		<List>
+			<EventListContent />
+		</List>
+	);
+};
+
+const EventListContent = () => {
+	const { isLoading } = useListContext();
+
+	if (isLoading) {
+		return (
+			<FlexBox sx={{ flexDirection: "column", justifyContent: "center", alignItems: "center", height: "80vh" }}>
+				<h2>Loading Events</h2>
+				<CircularProgress />
+			</FlexBox>
+		);
+	}
+
+	return (
+		<Datagrid bulkActionButtons={false}>
+			<TextField source="title" label="Title" />
+			<TextField source="slug" label="Slug" />
+			<DateField source="startDate" label="Start Date" showTime />
+			<TextField source="startTime" label="Start Time" />
+			<FunctionField
+				label="Actions"
+				render={() => {
+					return (
+						<FlexBox sx={{ justifyContent: "start", gap: 2 }}>
+							<DeleteButton />
+							<EditButton />
+						</FlexBox>
+					);
+				}}
+			/>
+			{/* <FunctionField
+				label="Status"
+				render={(event: Event) => {
+					return (
+						event.status[0].toUpperCase() +
+						event.status.slice(1).toLocaleLowerCase()
+					);
+				}}
+			/> */}
+		</Datagrid>
+	);
 };
--- src/operations/user/user-list.tsx
+++ src/operations/user/user-list.tsx
@@ -1,59 +1,74 @@
-import { Avatar } from "@mui/material";
+import { Avatar, CircularProgress } from "@mui/material";
 import {
-  List,
-  Datagrid,
-  TextField,
-  EmailField,
-  FunctionField,
-  DeleteButton,
-  EditButton,
+	List,
+	Datagrid,
+	TextField,
+	EmailField,
+	FunctionField,
+	DeleteButton,
+	EditButton,
+	useListContext,
 } from "react-admin";
 import { FlexBox } from "../../common/components/flex-box";
 import { User } from "../../providers";
 
 export const UserList = () => {
-  return (
-    <List resource="user">
-      <Datagrid bulkActionButtons={false}>
-        <FunctionField
-          label="Profile"
-          render={(user) => {
-            return <Avatar src={user.imageUrl} />;
-          }}
-        />
-        <TextField source="name" label="Name" />
-        <TextField source="username" label="Username" />
-        <EmailField source="email" label="Email" />
-        <FunctionField
-          label="Role"
-          render={(user: User) => {
-            return (
-              user?.roleId?.title[0].toUpperCase() +
-              user?.roleId?.title.slice(1)?.toLocaleLowerCase()
-            );
-          }}
-        />
+	return (
+		<List resource="user">
+			<UserListContent />
+		</List>
+	);
+};
 
-        <FunctionField
-          label="Contry"
-          render={(user: User) => {
-            return (
-              user?.countryId.name
-            );
-          }}
-        />
-        <FunctionField
-          label="Actions"
-          render={() => {
-            return (
-              <FlexBox sx={{ justifyContent: "start", gap: 2 }}>
-                <DeleteButton />
-                <EditButton />
-              </FlexBox>
-            );
-          }}
-        />
-      </Datagrid>
-    </List>
-  );
+const UserListContent = () => {
+	const { isLoading } = useListContext();
+
+	if (isLoading) {
+		return (
+			<FlexBox sx={{ flexDirection: "column", justifyContent: "center", alignItems: "center", height: "80vh" }}>
+				<h2>Loading Users</h2>
+				<CircularProgress />
+			</FlexBox>
+		);
+	}
+
+	return (
+		<Datagrid bulkActionButtons={false}>
+			<FunctionField
+				label="Profile"
+				render={(user) => {
+					return <Avatar src={user.imageUrl} />;
+				}}
+			/>
+			<TextField source="name" label="Name" />
+			<TextField source="username" label="Username" />
+			<EmailField source="email" label="Email" />
+			<FunctionField
+				label="Role"
+				render={(user: User) => {
+					return (
+						user?.roleId?.title[0].toUpperCase() +
+						user?.roleId?.title.slice(1)?.toLocaleLowerCase()
+					);
+				}}
+			/>
+			<FunctionField
+				label="Country"
+				render={(user: User) => {
+					return user?.countryId.name;
+				}}
+			/>
+			<FunctionField
+				label="Actions"
+				render={() => {
+					return (
+						<FlexBox sx={{ justifyContent: "start", gap: 2 }}>
+							<DeleteButton />
+							<EditButton />
+						</FlexBox>
+					);
+				}}
+			/>
+		</Datagrid>
+	);
 };

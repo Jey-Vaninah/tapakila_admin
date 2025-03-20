13a5bb8a102e1e82bb5adeca03d180fbe5df3991
tsilavina007 <andriamiart114@gmail.com>
2025-03-20 08:00:19 +0300

    🦄 refactor: default theme

--- src/app.tsx
+++ src/app.tsx
@@ -1,8 +1,8 @@
-import { Admin, CustomRoutes, Resource } from "react-admin";
+import { Admin, CustomRoutes, Resource, defaultTheme } from "react-admin";
 import {
-  Person as PersonIcon,
-  Celebration as CelebrationIcon,
-  LocalActivity as LocalActivityIcon,
+	Person as PersonIcon,
+	Celebration as CelebrationIcon,
+	LocalActivity as LocalActivityIcon,
 } from "@mui/icons-material";
 import { Route } from "react-router-dom";
 
@@ -26,43 +26,44 @@ import ProfilePage from "./operations/profil/profil-show";
 import ProfileEdit from "./operations/profil/profil-edit";
 
 export const App = () => {
-  return (
-    <Admin
-      requireAuth
-      layout={Layout}
-      loginPage={<LoginPage />}
-      title="Tapakila Admin"
-      dataProvider={dataProvider}
-      authProvider={authProvider}
-    >
-      <Resource
-        name="user"
-        icon={PersonIcon}
-        list={UserList}
-        show={UserShow}
-        edit={UserEdit}
-        create={UserCreate}
-      />
-      <Resource
-        name="event"
-        icon={CelebrationIcon}
-        list={EventList}
-        show={EventShow}
-        edit={EventEdit}
-        create={EventCreate}
-      />
-      <Resource
-        name="ticket"
-        icon={LocalActivityIcon}
-        list={TicketList}
-        show={TicketShow}
-        edit={TicketEdit}
-        create={TicketCreate}
-      />
-      <CustomRoutes>
-        <Route path="/profile" element={<ProfilePage />} />
-        <Route path="/profile/edit" element={<ProfileEdit />} />
-      </CustomRoutes>
-    </Admin>
-  );
+	return (
+		<Admin
+			requireAuth
+			layout={Layout}
+			loginPage={<LoginPage />}
+			title="Tapakila Admin"
+			dataProvider={dataProvider}
+			authProvider={authProvider}
+			theme={defaultTheme} // Ajoutez cette ligne pour définir le thème par défaut en mode clair
+		>
+			<Resource
+				name="user"
+				icon={PersonIcon}
+				list={UserList}
+				show={UserShow}
+				edit={UserEdit}
+				create={UserCreate}
+			/>
+			<Resource
+				name="event"
+				icon={CelebrationIcon}
+				list={EventList}
+				show={EventShow}
+				edit={EventEdit}
+				create={EventCreate}
+			/>
+			<Resource
+				name="ticket"
+				icon={LocalActivityIcon}
+				list={TicketList}
+				show={TicketShow}
+				edit={TicketEdit}
+				create={TicketCreate}
+			/>
+			<CustomRoutes>
+				<Route path="/profile" element={<ProfilePage />} />
+				<Route path="/profile/edit" element={<ProfileEdit />} />
+			</CustomRoutes>
+		</Admin>
+	);
 };

42a8f334c99220353614562a59634bd9136b7875
Jey-Vaninah <hei.vaninah@gmail.com>
2025-03-21 09:42:53 +0100

    refactor: create currency

--- src/operations/currency/currency-create.tsx
+++ src/operations/currency/currency-create.tsx
@@ -1,16 +1,11 @@
-import {
-  Create,
-  SimpleForm,
-  TextInput,
-  required,
-} from "react-admin";
+import { Create, SimpleForm, TextInput, required } from "react-admin";
 
 export const CurrencyCreate = () => (
   <Create>
     <SimpleForm>
       <TextInput source="id" readOnly />
       <TextInput source="title" validate={[required()]} />
-      <TextInput label="description" validate={[required()]} />
+      <TextInput source="description" validate={[required()]} />
     </SimpleForm>
   </Create>
 );

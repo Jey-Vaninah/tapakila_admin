68555a3ed5876eb33592ba886ad3d1e45667a6e4
tsilavina007 <andriamiart114@gmail.com>
2025-03-27 16:15:36 +0300

    ✨ feat: CREATE operation on Event

--- src/operations/event/event-create.tsx
+++ src/operations/event/event-create.tsx
@@ -6,31 +6,55 @@ import {
   TimeInput,
   required,
   SelectInput,
+  useGetList,
 } from "react-admin";
-import { v4 } from "uuid";
-import { Status } from "../../providers";
 
-export const EventCreate = () => (
-  <Create
-    transform={(data) => {
-      return { ...data, id: v4() };
-    }}
-  >
+export const EventCreate = () => {
+	  const { data: eventHalls = [] } = useGetList("eventHall");
+	  const { data: hosts = [] } = useGetList("host");
+	  const { data: users = [] } = useGetList("user");
+
+	  return (
+  <Create>
     <SimpleForm>
+	<SelectInput
+          label="User"
+          source="user.id"
+          choices={users.map((user) => ({
+            id: user.id,
+            name: user.name,
+          }))}
+          validate={[required()]}
+        />
       <TextInput source="title" validate={[required()]} />
       <TextInput source="slug" validate={[required()]} />
       <TextInput source="description" validate={[required()]} />
+	  <SelectInput
+          label="Host"
+          source="host.id"
+          choices={hosts.map((host) => ({
+            id: host.id,
+            name: host.name,
+          }))}
+          validate={[required()]}
+        />
+	  <SelectInput
+          label="Event Hall"
+          source="eventHall.id"
+          choices={eventHalls.map((event) => ({
+            id: event.id,
+            name: event.name,
+          }))}
+          validate={[required()]}
+        />
       <DateInput source="startDate" validate={[required()]} />
       <TimeInput source="startTime" validate={[required()]} />
-      <DateInput source="endDate" validate={[required()]} />
-      <TimeInput source="endTime" validate={[required()]} />
-      <TextInput source="ageLimit" validate={[required()]} />
-      <SelectInput
-        defaultValue={Status.CANCELED}
-        validate={[required()]}
-        source="status"
-        choices={[Status.CANCELED, Status.DRAFT, Status.PUBLISHED]}
-      />
+      <DateInput source="endDate"  />
+      <TimeInput source="endTime"  />
+      <TextInput source="ageLimit" />
+      <TextInput source="eventImage" />
+      <TextInput source="category" validate={[required()]} />
+
     </SimpleForm>
-  </Create>
-);
+  </Create> );
+};

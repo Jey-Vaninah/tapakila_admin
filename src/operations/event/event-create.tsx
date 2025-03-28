c41be554633d70679cac7610d24dbf4f7fddb494
tsilavina007 <andriamiart114@gmail.com>
2025-03-28 13:17:52 +0300

    🦄 refactor: refactor code

--- src/operations/event/event-create.tsx
+++ src/operations/event/event-create.tsx
@@ -6,80 +6,77 @@ import {
 	SelectInput,
 	required,
 	useGetList,
-  } from "react-admin";
+} from "react-admin";
 
-  // Fonction pour valider si l'heure est au bon format "HH:mm"
-  const validateTimeFormat = (value: string) => {
+const validateTimeFormat = (value: string) => {
 	if (!value) return 'L\'heure est requise.';
 
-	// Regex pour vérifier si l'heure correspond au format HH:mm
 	const timeRegex = /^([01]?[0-9]|2[0-3]):([0-5]?[0-9])$/;
 	if (!timeRegex.test(value)) {
-	  return 'L\'heure doit être au format HH:mm.';
+		return 'L\'heure doit être au format HH:mm.';
 	}
 
-	return undefined; // Pas d'erreur
-  };
+	return undefined;
+};
 
-  export const EventCreate = () => {
+export const EventCreate = () => {
 	const { data: eventHalls = [] } = useGetList("eventHall");
 	const { data: hosts = [] } = useGetList("host");
 	const { data: users = [] } = useGetList("user");
 
 	return (
-	  <Create>
-		<SimpleForm>
-		  <SelectInput
-			label="User"
-			source="user.id"
-			choices={users.map((user) => ({
-			  id: user.id,
-			  name: user.name,
-			}))}
-			validate={[required()]}
-		  />
-		  <TextInput source="title" validate={[required()]} />
-		  <TextInput source="slug" validate={[required()]} />
-		  <TextInput source="description" validate={[required()]} />
-		  <SelectInput
-			label="Host"
-			source="host.id"
-			choices={hosts.map((host) => ({
-			  id: host.id,
-			  name: host.name,
-			}))}
-			validate={[required()]}
-		  />
-		  <SelectInput
-			label="Event Hall"
-			source="eventHall.id"
-			choices={eventHalls.map((event) => ({
-			  id: event.id,
-			  name: event.name,
-			}))}
-			validate={[required()]}
-		  />
-		  <DateInput source="startDate" validate={[required()]} />
+		<Create>
+			<SimpleForm>
+				<SelectInput
+					label="User"
+					source="user.id"
+					choices={users.map((user) => ({
+						id: user.id,
+						name: user.name,
+					}))}
+					validate={[required()]}
+				/>
+				<TextInput source="title" validate={[required()]} />
+				<TextInput source="slug" validate={[required()]} />
+				<TextInput source="description" validate={[required()]} />
+				<SelectInput
+					label="Host"
+					source="host.id"
+					choices={hosts.map((host) => ({
+						id: host.id,
+						name: host.name,
+					}))}
+					validate={[required()]}
+				/>
+				<SelectInput
+					label="Event Hall"
+					source="eventHall.id"
+					choices={eventHalls.map((event) => ({
+						id: event.id,
+						name: event.name,
+					}))}
+					validate={[required()]}
+				/>
+				<DateInput source="startDate" validate={[required()]} />
 
-		  {/* Remplacement de TimeInput par TextInput avec validation manuelle */}
-		  <TextInput
-			source="startTime"
-			label="Start Time"
-			validate={[required(), validateTimeFormat]} // Validation pour vérifier le format
-		  />
+				<TextInput
+					source="startTime"
+					label="Start Time"
+					validate={[required(), validateTimeFormat]}
+				/>
 
-		  <DateInput source="endDate" />
+				<DateInput source="endDate" />
 
-		  <TextInput
-			source="endTime"
-			label="End Time"
-			validate={[validateTimeFormat]} // Validation pour vérifier le format de l'heure
-		  />
+				<TextInput
+					source="endTime"
+					label="End Time"
+					validate={[validateTimeFormat]}
+				/>
 
-		  <TextInput source="ageLimit" />
-		  <TextInput source="eventImage" />
-		  <TextInput source="category" validate={[required()]} />
-		</SimpleForm>
-	  </Create>
+				<TextInput source="ageLimit" />
+				<TextInput source="eventImage" />
+				<TextInput source="category" validate={[required()]} />
+			</SimpleForm>
+		</Create>
 	);
-  };
+};

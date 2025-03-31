0cc9b55d01047cd133169fd8747fb4794b33ddfc
tsilavina007 <andriamiart114@gmail.com>
2025-03-31 17:17:18 +0300

    🌈 style: Event List and Event Show

--- src/layout/layout.tsx
+++ src/layout/layout.tsx
@@ -2,10 +2,8 @@ import { Box } from "@mui/material";
 import { FC, ReactNode } from "react";
 import { Menu } from "./menu";
 import { AppBar } from "./appbar";
-import { useTheme } from "react-admin";
 
 export const Layout: FC<{ children: ReactNode }> = ({ children }) => {
-  const [theme] = useTheme();
 
   return (
     <Box
@@ -13,7 +11,7 @@ export const Layout: FC<{ children: ReactNode }> = ({ children }) => {
         width: "100%",
         height: "100vh",
         overflowY: "scroll",
-        bgcolor: theme === "dark" ? "#383838" : "#f3f3f3",
+        bgcolor: "#f9fafb",
       }}
     >
       <Menu />
